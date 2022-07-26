602aa24f8189b8c7b53bbdc75b8d1d169d4ccbdd
Jessica Morrison <jnicomorr@gmail.com>
2022-07-25 19:42:36 -0500

    Added cards to team page

--- pages/team.js
+++ pages/team.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getPlayers } from '../api/playerData';
+import PlayerCard from '../components/PlayerCard';
+import { useAuth } from '../utils/context/authContext';
 
 export default function Team() {
+  const [players, setPlayers] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getPlayers(user.uid).then(setPlayers);
+  },
+  [user.uid]);
+
   return (
-    <div>
-      <h1>Team</h1>
+    <div className="text-center my-4">
+      <Link href="/player/new" passHref>
+        <Button>Add A Player</Button>
+      </Link>
+      {players.map((player) => (
+        <PlayerCard key={player.firebaseKey} playerObj={player} onUpdate={getPlayers} />
+      ))}
+      <div className="d-flex flex-wrap" />
     </div>
   );
 }
